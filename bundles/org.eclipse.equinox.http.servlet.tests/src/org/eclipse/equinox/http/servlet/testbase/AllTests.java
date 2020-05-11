42505e736f0e64a526e278c0bd49de55a6e5045f
Raymond Augé <raymond.auge@liferay.com>
2020-05-11 11:41:47 -0400

    Bug 562843 - Do not decode already decoded servletPaths
    Signed-off-by: Raymond Augé <raymond.auge@liferay.com>

--- bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
+++ bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
@@ -15,6 +15,7 @@ package org.eclipse.equinox.http.servlet.testbase;
 
 import org.eclipse.equinox.http.servlet.tests.AuthenticationTest;
 import org.eclipse.equinox.http.servlet.tests.Bug500783_Test;
+import org.eclipse.equinox.http.servlet.tests.Bug562843_2_Test;
 import org.eclipse.equinox.http.servlet.tests.Bug562843_Test;
 import org.eclipse.equinox.http.servlet.tests.Bug564747_Test;
 import org.eclipse.equinox.http.servlet.tests.ContextHelperCustomizerTests;
@@ -100,6 +101,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	ContextHelperCustomizerTests.class,
 	Bug500783_Test.class,
 	Bug562843_Test.class,
+	Bug562843_2_Test.class,
 	Bug564747_Test.class
 })
 public class AllTests {
