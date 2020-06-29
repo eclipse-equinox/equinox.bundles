63fdaf56c3dc4445e2ae9af3185f5835928d3cd4
Thomas Watson <tjwatson@us.ibm.com>
2020-06-29 13:40:59 -0500

    Bug 564747 - Fix empty query params to use the empty string
    Fix the query string parameter map parsing treat empty params to be set
    to the empty string.
    
    Change-Id: Ie86757fa0a440bda42f32e5830a1de34964f6fe7
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
+++ bundles/org.eclipse.equinox.http.servlet.tests/src/org/eclipse/equinox/http/servlet/testbase/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
+ * Copyright (c) 2016, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import org.eclipse.equinox.http.servlet.tests.Bug500783_Test;
 import org.eclipse.equinox.http.servlet.tests.Bug562440_Test;
 import org.eclipse.equinox.http.servlet.tests.Bug562843_2_Test;
 import org.eclipse.equinox.http.servlet.tests.Bug562843_Test;
+import org.eclipse.equinox.http.servlet.tests.Bug564747_Test;
 import org.eclipse.equinox.http.servlet.tests.DispatchingTest;
 import org.eclipse.equinox.http.servlet.tests.ServletTest;
 import org.junit.runner.RunWith;
@@ -27,7 +28,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	Bug500783_Test.class,
 	Bug562440_Test.class,
 	Bug562843_Test.class,
-	Bug562843_2_Test.class
+	Bug562843_2_Test.class,
+	Bug564747_Test.class
 })
 public class AllTests {
 	// see @SuiteClasses
